2cce258eee08976c019003dbe430b07e20ea2556
Hamza <hamzadanden2@gmail.com>
2019-10-18 10:57:16 +0200

    warmUp3 done

--- warmUp3.js
+++ warmUp3.js
@@ -1,8 +1,23 @@
 // 1-Create arrays in the global scope consisting of strings that represent:
 // -name of females in your class
+var females = ['ons', 'lina', 'henia'];
 // -name of males in your class
+var males = ['amine', 'ahmed', 'taha']
 // -name of your class instructors 
-
+var instructors = ['youssef', 'seif', 'matt']
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
-
+var middle = function(array) {
+  var x = array.length - 1
+  if (x / 2 === 1) {
+	  return array[x / 2]
+  }else{
+    return array[Math.floor(x / 2)]
+  }
+}
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+var x2 = function(array) {
+  for(var i = 0; i < array.length; i += 2){
+    array[i] *= 2;
+  }
+  return array;
+}
