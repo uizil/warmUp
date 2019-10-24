868ce6dc92a957b8009dc3e679f3fed9c1f340a0
Hamza <hamzadanden2@gmail.com>
2019-10-24 10:44:12 +0200

    warmUp7 done

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,17 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+var partnerFirstName = 'Dhia';
+var partnerSecondName  = 'Weslati'
+var partnerFullName = partnerFirstName + ' ' + partnerSecondName;
+
+var multiple = (13 % 3 === 0);
+
+var arrayOfAges = [13,14,13,15,16,17,19,13,16,15];
+var averageAges = 0;
+for(var i = 0; i <arrayOfAges.length; i++) {
+	averageAges += arrayOfAges[i];
+}
+averageAges = averageAges / arrayOfAges.length;
+
+var myAgeInSeconds = 19 * 365 * 24 * 60 * 60;
