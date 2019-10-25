07b29326f64b11fc4ef4372fd83ad5faf1e5caf8
Hamza <hamzadanden2@gmail.com>
2019-10-25 10:59:20 +0200

    warmUp8 done

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,20 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+
+function subset(array, sub) {
+	var sub1 = []; 
+	
+	for(var i = 0; i < array.length; i++) {
+		
+		for (var j = 0; j < sub.length; j++){
+			
+			if(array[i] === sub[j]){
+				sub1.push(1);
+			}
+		}
+	}
+	
+	return sub1.length === sub.length;
+}
