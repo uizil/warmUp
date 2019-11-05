022547544f7e34ec7d2e49c84d52cd005523a5e6
Hamza <hamzadanden2@gmail.com>
2019-11-05 09:53:20 +0100

    warmUp 15 done

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,27 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverse(string) {
+	var words = '';
+	result = '';
+			var i = string.length - 1;
+			var j = string.length - 1;
+		 	while (i >= 0){
+		 		while (string[j] !== ' ') {
+					words = string[j] + words
+					j--;
+				}
+				if (string[j] === ' ') {
+					j = i;
+				}
+				result = words + ' ' +result;
+
+				
+		 		i -= 1;
+		 	}
+		
+		
+
+	return result;
+}
